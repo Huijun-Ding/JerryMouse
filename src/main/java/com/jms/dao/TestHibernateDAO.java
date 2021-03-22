5463cba98c8bc435045f036f69b0a2eda5686beb
Huijun <mathilda.cnfr@gmail.com>
2021-03-22 18:21:20 +0100

    add Hibernate

---
+++ src/main/java/com/jms/dao/TestHibernateDAO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jms.dao;
+
+import java.text.ParseException;
+
+/**
+ *
+ * @author Mathi
+ */
+public class TestHibernateDAO {
+     
+    /**
+     * Programme de test.
+     */
+    public static void main(String[] args) throws ParseException {
+        /*----- Test -----*/
+
+        //TestHibernateDAO.xxxxxxx();
+
+        /*----- Exit -----*/
+        System.exit(0);
+    }
+    
+}
