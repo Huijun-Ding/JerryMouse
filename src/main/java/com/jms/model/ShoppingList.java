5a9385acf5e0f23489b2e025b415bbc830c990dc
Miangola <rakotoarisoamiangola@gmail.com>
2021-03-23 11:37:32 +0100

    Update ShoppingList.java
    Add Hibernate annotations and javadoc

--- src/main/java/com/jms/model/ShoppingList.java
+++ src/main/java/com/jms/model/ShoppingList.java
@@ -1,15 +1,55 @@
 package com.jms.model;
 
-/**
- *
- * @author carol
- */
-public class ShoppingList {
-    //proprieties
-   private int id;
-   private String name;
-   //constructor
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+/**
+ * ShoppingList Class
+ * @author JerryMouseSoftware
+ */
+
+@Entity(name="ListeCourse")
+public class ShoppingList {
+
+    //--------------- Properties -----------------
+    
+    /**
+     * Designation: Unique code identifying a Shopping list
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="CodeLC")
+    private int id;
+    
+    /**
+     * Designation: Name of a shopping list
+     */
+    @Column(name="NomLC")
+    private String name;
+    
+    /**
+     * Designation: A REMPLIR!!!!
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="CodeClient")
+    private Client client;
+    
+    /**
+     * Designation: A REMPLIR!!!!
+     */
+    @OneToMany(mappedBy ="shoppingList", fetch = FetchType.LAZY)
+    private Set<PostIt> postIts = new HashSet(0);
+    
+    //---------------- Constructor -----------------
     public ShoppingList() {
     }
 
@@ -17,38 +57,62 @@ public class ShoppingList {
         this.id = id;
         this.name = name;
     }
-   
-    //getter/setter
 
+    //---------------- Getters & Setters -----------------------
+    /**
+     * Getter for the property id.
+     * @return the id property. 
+     */
     public int getId() {
         return id;
     }
-
+    /**
+     * Setter for the property id.
+     * @param id The new value to set to the property.
+     */
     public void setId(int id) {
         this.id = id;
     }
-
+    
+    /**
+     * Getter for the property name.
+     * @return the name property. 
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Setter for the property name.
+     * @param name The new value to set to the property.
+     */
     public void setName(String name) {
         this.name = name;
     }
-    //Override methods
-
+    //--------------------- Methods -------------------
+    /**
+     * Method which converts the current object into a String object.
+     */
     @Override
     public String toString() {
         return "ShoppingList{" + "id=" + id + ", name=" + name + '}';
     }
-
+    
+    /**
+     * Method which returns the hash code of the current object id.
+     * @return The hash code of the current object id.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 19 * hash + this.id;
         return hash;
     }
-
+    
+    /**
+     * Method which compares the current object with another one.
+     * @param obj The object to compare with.
+     * @return <b>True</b> if both objects are equals, <b>False</b> else.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -66,5 +130,5 @@ public class ShoppingList {
         }
         return true;
     }
-    
+
 }
