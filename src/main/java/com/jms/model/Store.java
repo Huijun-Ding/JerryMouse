572e915ffc1fe84c02376aafa1eb3175141af27a
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-03-23 11:47:01 +0100

    Update Store.java
    Ajout de la java doc.

--- src/main/java/com/jms/model/Store.java
+++ src/main/java/com/jms/model/Store.java
@@ -1,71 +1,162 @@
 package com.jms.model;
+
 /**
- * 
- * @author carol
+ * Store Class.
+ * @author Jerry Mouse Software.
  */
 public class Store {
-//properities
-    private int id;
-    private String name;
-    private String street;
-    private String city;
-    private String postalCode;
-// constructor
-    public Store() {
-    }
 
+    // --------------------- PROPERTIES ---------------------
+
+    /**
+     * Unique code identifying a store.
+     * <b>Rule(s) : > 0</b>.
+     */
+    private int id;
+
+    /**
+     * Name of a store.
+     * <b>Maximum size : 50</b>
+     */
+    private String name;
+
+    /**
+     * Number and type of street where a store is located.
+     * <b>Rule(s) : > 0</b>.
+     * <b>Format : street_number, street_type street_name</b>
+     * <b>Maximum size : 100</b>
+     */
+    private String street;
+
+    /**
+     * City where a store is located.
+     * <b>Maximum size : 100</b>
+     */
+    private String city;
+
+    /**
+     * Postal code of the place where a store is located.
+     * <b>Maximum size : 5</b>
+     */
+    private String postalCode;
+
+    // -------------------- CONSTRUCTORS --------------------
+
+    /**
+     * A constructor of the Store Class.
+     */
+    public Store() { }
+
+    /**
+     * A constructor of the Store Class.
+     * @param name Name of a store.
+     * @param street Number and type of street where a store is located.
+     * @param city City where a store is located.
+     * @param postalCode Postal code of the place where a store is located.
+     */
     public Store(String name, String street, String city, String postalCode) {
         this.name = name;
         this.street = street;
         this.city = city;
         this.postalCode = postalCode;
     }
-// getter / setter
+
+    // ----------------- GETTERS & SETTERS ------------------
+    
+    /**
+     * Getter for the id property.
+     * @return The id property.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for the id property.
+     * @param id The new value to set to the property.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for the name property.
+     * @return The name property.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for the name property.
+     * @param name The new value to set to the property.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for the street property.
+     * @return The street property.
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Setter for the street property.
+     * @param street The new value to set to the property.
+     */
     public void setStreet(String street) {
         this.street = street;
     }
-
+    
+    /**
+     * Getter for the city property.
+     * @return The city property.
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Setter for the city property.
+     * @param city The new value to set to the property.
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Getter for the postal code property.
+     * @return The postal code property.
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /**
+     * Setter for the postal code property.
+     * @param postalCode The new value to set to the property.
+     */
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
-// Override methods
+
+    // ----------------------- METHODS ----------------------
+
+    /**
+     * Method which converts the current object into a String object.
+     */
     @Override
     public String toString() {
         return "Store{" + "id=" + id + ", name=" + name + ", street=" + street + ", city=" + city + ", postalCode=" + postalCode + '}';
     }
 
+    /**
+     * Method which returns the hash code of the current object id.
+     * @return The hash code of the current object id.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -73,6 +164,11 @@ public class Store {
         return hash;
     }
 
+    /**
+     * Method which compares the current object with another one.
+     * @param obj The object to compare with.
+     * @return <b>True</b> if both objects are equals, <b>False</b> else.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -90,7 +186,4 @@ public class Store {
         }
         return true;
     }
-    
-    
-
 }
