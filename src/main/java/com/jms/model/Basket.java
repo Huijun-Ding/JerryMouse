4137d8347638f2b91cbeb9b513a588d389d708dd
Shanshan-ZHAO <13051105108@163.com>
2021-03-23 12:33:04 +0100

    javadoc

--- src/main/java/com/jms/model/Basket.java
+++ src/main/java/com/jms/model/Basket.java
@@ -10,58 +10,126 @@ import javax.persistence.ManyToOne;
 
 /**
  * Basket Class.
+ *
  * @author Jerry Mouse Software.
  */
 @Entity
-public class Basket implements Serializable{
+public class Basket implements Serializable {
     // --------------------- PROPERTIES ---------------------
+
     @EmbeddedId
     private BasketId basketId;
-    
-    @Column (name = "qtePanier")
+    /**
+     * Quantity of a product in a given customer's cart.
+     * <b>Rule(s) : QtePanier</b>
+     * <b>Rule(s) : > 0</b>
+     */
+    @Column(name = "qtePanier")
     private int qtyBasket;
 
     //-----------Relation with Product--------------
-    @ManyToOne 
+    @ManyToOne
     @JoinColumn(name = "EANP", insertable = false, updatable = false)
     private Product product;
-    
+
     //-----------Relation with Client--------------
-    @ManyToOne 
+    @ManyToOne
     @JoinColumn(name = "CodeCL", insertable = false, updatable = false)
     private Client client;
-    
+
     // -------------------- CONSTRUCTORS --------------------
     public Basket() {
     }
 
+    /**
+     * A constructor of the Basket Class.
+     * @param basketId id of basket.
+     * @param qtyBasket Quantity of a product in a given customer's cart.
+     */
     public Basket(BasketId basketId, int qtyBasket) {
         this.basketId = basketId;
         this.qtyBasket = qtyBasket;
     }
-    
+
     // ----------------- GETTERS & SETTERS ------------------
+    /**
+     * Getter for the basketId property.
+     * @return The basketId property.
+     */
     public BasketId getBasketId() {
         return basketId;
     }
+
+    /**
+     * Setter for the basketId property.
+     * @param basketId The new value to set to the property.
+     */
 
     public void setBasketId(BasketId basketId) {
         this.basketId = basketId;
     }
 
+    /**
+     * Getter for the qtyBasket property.
+     * @return The qtyBasket property.
+     */
+
     public int getQtyBasket() {
         return qtyBasket;
     }
 
+    /**
+     * Setter for the qtyBasket property.
+     * @param qtyBasket The new value to set to the property.
+     */
+
     public void setQtyBasket(int qtyBasket) {
         this.qtyBasket = qtyBasket;
     }
-    
+    /**
+     * Getter for the product property.
+     * @return The product property.
+     */
+    public Product getProduct() {
+        return product;
+    }
+    /**
+     * Setter for the product property.
+     * @param product The new value to set to the property.
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    /**
+     * Getter for the client property.
+     * @return The client property.
+     */
+    public Client getClient() {
+        return client;
+    }
+    /**
+     * Setter for the client property.
+     * @param client The new value to set to the property.
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
     // ----------------------- METHODS ------------------------
     @Override
+    /**
+     * Method which converts the current object into a String object.
+     */
+
     public String toString() {
         return "Basket{" + "basketId=" + basketId + ", qtyBasket=" + qtyBasket + '}';
     }
+
+    /**
+     * Method which returns the hash code of the current object id.
+     *
+     * @return The hash code of the current object id.
+     */
 
     @Override
     public int hashCode() {
@@ -69,6 +137,12 @@ public class Basket implements Serializable{
         hash = 59 * hash + Objects.hashCode(this.basketId);
         return hash;
     }
+
+    /**
+     * Method which compares the current object with another one.
+     * @param obj The object to compare with.
+     * @return <b>True</b> if both objects are equals, <b>False</b> else.
+     */
 
     @Override
     public boolean equals(Object obj) {
@@ -87,5 +161,5 @@ public class Basket implements Serializable{
         }
         return true;
     }
-    
+
 }
--- src/main/java/com/jms/model/BasketId.java
+++ src/main/java/com/jms/model/BasketId.java
@@ -7,53 +7,105 @@ import javax.persistence.Entity;
 
 /**
  * BasketId Class.
+ *
  * @author Jerry Mouse Software.
  */
 @Embeddable
-public class BasketId implements Serializable{
+public class BasketId implements Serializable {
+
     // --------------------- PROPERTIES ---------------------
-    private int id; //id of custume
-    private String ean;
-    
+    /**
+     * Unique code identifying a customer.
+     * <b>Rule(s) : > 0</b>
+     */
+    private int id;
+    /**
+     * EAN (European Article Numbering): Bar code that uniquely identifies a
+     * product.
+     * <b>Format : 12 or 13 digits</b>
+     * <b>Maximum size : 20</b>
+     */
+    private String eanp;
+
     // -------------------- CONSTRUCTORS --------------------
     public BasketId() {
     }
 
-    public BasketId(int id, String ean) {
+    /**
+     * A constructor of the BasketId Class.
+     *
+     * @param id Unique code identifying a customer.
+     * @param eanp EAN (European Article Numbering): Bar code that uniquely
+     * identifies a product.
+     */
+    public BasketId(int id, String eanp) {
         this.id = id;
-        this.ean = ean;
+        this.eanp = eanp;
     }
-    
+
     // ----------------- GETTERS & SETTERS ------------------
+    /**
+     * Getter for the id property.
+     *
+     * @return The id property.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for the id property.
+     *
+     * @param id The new value to set to the property.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getEan() {
-        return ean;
+    /**
+     * Getter for the eanp property.
+     *
+     * @return The eanp property.
+     */
+    public String getEanp() {
+        return eanp;
     }
 
-    public void setEan(String ean) {
-        this.ean = ean;
+    /**
+     * Setter for the eanp property.
+     *
+     * @param eanp The new value to set to the property.
+     */
+    public void setEanp(String eanp) {
+        this.eanp = eanp;
     }
-    
+
     // ----------------------- METHODS ------------------------
+    /**
+     * Method which converts the current object into a String object.
+     */
     @Override
     public String toString() {
-        return "BasketId{" + "id=" + id + ", ean=" + ean + '}';
+        return "BasketId{" + "id=" + id + ", ean=" + eanp + '}';
     }
 
+    /**
+     * Method which returns the hash code of the current object id.
+     * @return The hash code of the current object id.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 59 * hash + this.id;
-        hash = 59 * hash + Objects.hashCode(this.ean);
+        hash = 59 * hash + Objects.hashCode(this.eanp);
         return hash;
     }
+
+    /**
+     * Method which compares the current object with another one.
+     * @param obj The object to compare with.
+     * @return <b>True</b> if both objects are equals, <b>False</b> else.
+     */
 
     @Override
     public boolean equals(Object obj) {
@@ -70,7 +122,7 @@ public class BasketId implements Serializable{
         if (this.id != other.id) {
             return false;
         }
-        if (!Objects.equals(this.ean, other.ean)) {
+        if (!Objects.equals(this.eanp, other.eanp)) {
             return false;
         }
         return true;
