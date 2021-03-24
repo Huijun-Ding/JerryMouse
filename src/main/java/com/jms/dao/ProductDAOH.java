d35f65053b82df57cd98b5751ab1cb3909555276
malaikateuhi <malaikateuhi@gmail.com>
2021-03-24 15:34:57 +0100

    Add class ProductDAO with hibernate with methods
    get one product from ID
    get all products for displaying in list

---
+++ src/main/java/com/jms/dao/ProductDAOH.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 mlk.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jms.dao;
+
+import com.jms.model.Product;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Jerry Mouse Software
+ */
+public class ProductDAOH {
+
+    // ----------------------- METHODS ----------------------
+    /**
+     * Gets all the information of a product from its EAN.
+     *
+     * @param id the ean of the product.
+     */
+    public static void getProduct(String id) {
+        /*----- Session opening -----*/
+        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+
+            //System.out.println("--------- GET ONE PRODUCT FROM ID ");
+            Product prod = session.get(Product.class, id);
+            System.out.println(prod);
+
+        }
+    }
+
+    /**
+     * Gets all the products in the catalog.
+     * @return the list of the products.
+     */
+    public static List<Product> getAllProducts() {
+        List<Product> list;
+        /*----- Session opening -----*/
+        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+
+            //System.out.println("--------- GET ALL PRODUCTS ");
+            
+            String sql = 
+                    "SELECT new com.jms.model.Product(p.ean, p.name, p.format, "
+                    + "p.nutriscore, p.packaging, p.packagingQuantity, "
+                    + "p.unitPrice, p.kgPrice, p.urlThumbnail) "
+                    + "FROM Product p";
+            list = session.createQuery(sql).list();
+            return list;
+        }
+    }
+
+}
