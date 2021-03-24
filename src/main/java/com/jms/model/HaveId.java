93c41903240a9fccbe753bf2d53761a0afcac79d
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-03-24 16:21:40 +0100

    Update HaveId.java
    Ajout de la javadoc

--- src/main/java/com/jms/model/HaveId.java
+++ src/main/java/com/jms/model/HaveId.java
@@ -7,24 +7,39 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 /**
- *
- * @author carol
+ * HaveId Class.
+ * @author Jerry Mouse Software.
  */
 @Embeddable
 public class HaveId implements Serializable {
-    
+
     // --------------------- PROPERTIES ---------------------
+
+    /**
+     * Hibernate join property with TimeSlot Class.
+     */
     @Column(name = "HeureDebutCR")
     private Date startTime;
-    
+
+    /**
+     * Hibernate join property with Store Class.
+     */
     @Column(name = "CodeM")
     private int storeId;
-    
+
     // -------------------- CONSTRUCTORS --------------------
 
+    /**
+     * A constructor of the HaveId Class.
+     */
     public HaveId() {
     }
-    
+
+    /**
+     * A constructor of the HaveId Class.
+     * @param startTime Start time of a time slot.
+     * @param storeId Unique code identifying a store.
+     */
     public HaveId(Date startTime, int storeId) {
         this.startTime = startTime;
         this.storeId = storeId;
@@ -32,29 +47,52 @@ public class HaveId implements Serializable {
 
     // ----------------- GETTERS & SETTERS ------------------
     
+    /**
+     * Getter for the startTime property.
+     * @return The startTime property.
+     */
     public Date getStartTime() {
         return startTime;
     }
-
+    
+    /**
+     * Setter for the startTime property.
+     * @param startTime The new value to set to the property.
+     */
     public void setStartTime(Date startTime) {
         this.startTime = startTime;
     }
-
+    
+    /**
+     * Getter for the storeId property.
+     * @return The storeId property.
+     */
     public int getStoreId() {
         return storeId;
     }
-
+    
+    /**
+     * Setter for the storeId property.
+     * @param storeId The new value to set to the property.
+     */
     public void setStoreId(int storeId) {
         this.storeId = storeId;
     }
     
     // ----------------------- METHODS ----------------------
 
+    /**
+     * Method which converts the current object into a String object.
+     */
     @Override
     public String toString() {
         return "HaveId{" + "startTime=" + startTime + ", storeId=" + storeId + '}';
     }
 
+    /**
+     * Method which returns the hash code of the current object id.
+     * @return The hash code of the current object id.
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -62,7 +100,12 @@ public class HaveId implements Serializable {
         hash = 67 * hash + this.storeId;
         return hash;
     }
-
+    
+    /**
+     * Method which compares the current object with another one.
+     * @param obj The object to compare with.
+     * @return <b>True</b> if both objects are equals, <b>False</b> else.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -83,5 +126,4 @@ public class HaveId implements Serializable {
         }
         return true;
     }
-    
 }
