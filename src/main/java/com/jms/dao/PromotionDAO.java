67b36663ed68700916ec8aa3f2703a53a617ad26
Shanshan-ZHAO <13051105108@163.com>
2021-03-25 10:36:08 +0100

    ajouter une méthode pour chercher une promotion

--- src/main/java/com/jms/dao/PromotionDAO.java
+++ src/main/java/com/jms/dao/PromotionDAO.java
@@ -6,6 +6,7 @@
 package com.jms.dao;
 
 import com.jms.model.Basket;
+import com.jms.model.Promotion;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -16,6 +17,25 @@ import org.hibernate.query.Query;
  * @author Jerry Mouse Software.
  */
 public class PromotionDAO {
+    public static Promotion searchPromotion(String ean){
+        /*----- Ouverture de la session -----*/
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+            Query query = session.createSQLQuery("Select Promotion.*"
+                    + "from Reduce, Promotion "
+                    + "where Reduce.CodePR = Promotion.CodePR"
+                    + "and unix_timestamp(now()) between unix_timestamp(Reduce.DateDebutPromoP) and unix_timestamp(Reduce.DateFinPromoP)"
+                    + "and Reduce.EANP = :ean");
+
+            query.setParameter("ean", ean);
+
+            Promotion promotion = (Promotion)query.list().get(0);
+            
+            t.commit(); // Commit et flush automatique de la session.
+            return promotion;
+        }
+    }
+    
     public static float getPercentage(String ean){
         /*----- Ouverture de la session -----*/
         try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
