8670d8761b1e7e0236f334f08f6f8095c210b0b6
Huijun <mathilda.cnfr@gmail.com>
2021-03-25 16:34:22 +0100

    addEventListener

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -18,10 +18,20 @@ function getSearchElement() {
                 elt = document.getElementById("search_result");
                 if (suggestions != null && suggestions.length != 0) {
                     elt.style.display = "block";
+
                     elt.innerHTML = "";
+
+                    var text = "";
                     for (i = 0; i < suggestions.length; i++) {
-                        prod = suggestions[i].firstChild.nodeValue;
-                        elt.insertAdjacentHTML("beforeend", "<li class='list-group-item'>" + prod + "</li>");
+                        var prod = suggestions[i].firstChild.nodeValue;
+                        text += "<li class='list-group-item' class='elements'>" + prod + "</li>";
+
+                        elt.innerHTML = text;
+
+                        var resultSize = document.getElementsByClassName("elements").length;
+                        var nodes = document.getElementsByClassName("elements");
+                        for (var i = 0; i < resultSize; i++)
+                            nodes[i].addEventListener("click", searchProductsBySuggestion(nodes[i]));
                     }
                 } else {
                     elt.style.display = "none";
@@ -62,7 +72,36 @@ function searchProducts() {
             }
         }
     };
+    xhr.send();
+}
 
+function searchProductsBySuggestion() {
+    var myinput = encodeURIComponent(this.value);
+    alert(myinput);
+    // Objet XMLHttpRequest.
+    var xhr = new XMLHttpRequest();
+ 
+    xhr.open("GET", "../SendSearchRequestServlet?keyword=" + myinput);
+
+    xhr.onload = function ()
+    {
+        if (xhr.status === 200)
+        {
+            alert(xhr.status);
+            suggestions = xhr.responseXML.getElementsByTagName("product");
+            elt = document.getElementById("products");
+            if (suggestions != null && suggestions.length != 0) {
+                elt.innerHTML = "";
+                for (i = 0; i < suggestions.length; i++) {
+                    produit = suggestions[i].firstChild.nodeValue;
+
+                    elt.insertAdjacentHTML("beforeend", "<li>" + produit + "</li>");
+                }
+            } else {
+                elt.insertAdjacentHTML("beforeend", "<li>Il n'y a pas de produit correspondant.</li>");
+            }
+        }
+    };
     xhr.send();
 }
 
@@ -84,5 +123,3 @@ document.addEventListener("DOMContentLoaded", () => {
 //
 //    document.getElementById("bt_connect").addEventListener("click", connection);
 //});
-
-
