3713cc35bd8a2c5fbf7b214fd698cfe905cf8e3e
Shanshan-ZHAO <13051105108@163.com>
2021-03-25 16:35:42 +0100

    ajouter méthode searchPromotion

--- src/main/java/com/jms/dao/PromotionDAO.java
+++ src/main/java/com/jms/dao/PromotionDAO.java
@@ -7,6 +7,7 @@ package com.jms.dao;
 
 import com.jms.model.Basket;
 import com.jms.model.Promotion;
+import java.sql.SQLException;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -17,41 +18,55 @@ import org.hibernate.query.Query;
  * @author Jerry Mouse Software.
  */
 public class PromotionDAO {
-    public static Promotion searchPromotion(String ean){
+
+    public static Promotion searchPromotion(String ean) throws SQLException {
         /*----- Ouverture de la session -----*/
         try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             Transaction t = session.beginTransaction();
-            Query query = session.createSQLQuery("Select Promotion.*"
-                    + "from Reduce, Promotion "
-                    + "where Reduce.CodePR = Promotion.CodePR"
-                    + "and unix_timestamp(now()) between unix_timestamp(Reduce.DateDebutPromoP) and unix_timestamp(Reduce.DateFinPromoP)"
-                    + "and Reduce.EANP = :ean");
+            System.out.println(ean);
+            Query query = session.createSQLQuery("Select Promotion.* "
+                    + "from Reduire, Promotion "
+                    + "where Reduire.CodePR = Promotion.CodePR "
+                    + "and CURRENT_DATE() >= Reduire.DateDebutPromoP "
+                    + "and CURRENT_DATE() <= Reduire.DateFinPromoP "
+                    + "and Reduire.EANP = :ean").addEntity(Promotion.class);
 
             query.setParameter("ean", ean);
-
-            Promotion promotion = (Promotion)query.list().get(0);
-            
-            t.commit(); // Commit et flush automatique de la session.
+//            System.out.println("test");
+            System.out.println(query.list().get(0));
+            Promotion promotion = (Promotion) query.list().get(0);
+//            System.out.println("test1");
+            t.commit();
             return promotion;
         }
     }
-    
-    public static float getPercentage(String ean){
-        /*----- Ouverture de la session -----*/
-        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
-            Transaction t = session.beginTransaction();
-            Query query = session.createSQLQuery("Select Promotion.PercentagePR"
-                    + "from Reduce, Promotion "
-                    + "where Reduce.CodePR = Promotion.CodePR"
-                    + "and unix_timestamp(now()) between unix_timestamp(Reduce.DateDebutPromoP) and unix_timestamp(Reduce.DateFinPromoP)"
-                    + "and Reduce.EANP = :ean");
 
-            query.setParameter("ean", ean);
-
-            float percentage = (float)query.list().get(0);
-            
-            t.commit(); // Commit et flush automatique de la session.
-            return percentage;
+    //    public static float getPercentage(String ean){
+//        /*----- Ouverture de la session -----*/
+//        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+//            Transaction t = session.beginTransaction();
+//            Query query = session.createSQLQuery("Select Promotion.PercentagePR"
+//                    + "from Reduce, Promotion "
+//                    + "where Reduce.CodePR = Promotion.CodePR"
+////                    + "and unix_timestamp(now()) between unix_timestamp(Reduce.DateDebutPromoP) and unix_timestamp(Reduce.DateFinPromoP)"
+//                    + "and CURRENT_DATE() >= Reduire.DateDebutPromoP "
+//                    + "and CURRENT_DATE() <= Reduire.DateFinPromoP"
+//
+//                    + "and Reduce.EANP = :ean");
+//
+//            query.setParameter("ean", ean);
+//
+//            float percentage = (float)query.list().get(0);
+//            
+//            t.commit(); // Commit et flush automatique de la session.
+//            return percentage;
+//        }
+//    }
+    public static void main(String[] args) {
+        try {
+            System.out.println(PromotionDAO.searchPromotion("P1"));
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
