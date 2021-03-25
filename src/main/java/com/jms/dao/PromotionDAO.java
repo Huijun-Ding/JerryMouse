bff7f6c4787f2e37374f8542fc7875f7df539854
Shanshan-ZHAO <13051105108@163.com>
2021-03-25 01:04:52 +0100

    search percentage of promotion for un product

---
+++ src/main/java/com/jms/dao/PromotionDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jms.dao;
+
+import com.jms.model.Basket;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+/**
+ *
+ * @author Jerry Mouse Software.
+ */
+public class PromotionDAO {
+    public static float getPercentage(String ean){
+        /*----- Ouverture de la session -----*/
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+            Query query = session.createSQLQuery("Select Promotion.PercentagePR"
+                    + "from Reduce, Promotion "
+                    + "where Reduce.CodePR = Promotion.CodePR"
+                    + "and unix_timestamp(now()) between unix_timestamp(Reduce.DateDebutPromoP) and unix_timestamp(Reduce.DateFinPromoP)"
+                    + "and Reduce.EANP = :ean");
+
+            query.setParameter("ean", ean);
+
+            float percentage = (float)query.list().get(0);
+            
+            t.commit(); // Commit et flush automatique de la session.
+            return percentage;
+        }
+    }
+}
