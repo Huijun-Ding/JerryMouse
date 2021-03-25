4b88fda03e4b22681b58fadcf79f9ba4bc457144
Shanshan-ZHAO <13051105108@163.com>
2021-03-25 18:31:29 +0100

    nouveau servlet pour ajouter un product dans le panier

---
+++ src/main/java/com/jms/controller/AddProductServlet.java
@@ -0,0 +1,82 @@
+package com.jms.controller;
+
+import com.jms.dao.BasketDAO;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jerry Mouse Software.
+ */
+public class AddProductServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int idClient = Integer.parseInt(request.getParameter("idClient"));
+//        int idClient = 1;
+        String ean = request.getParameter("ean");
+        
+        try {
+            if (BasketDAO.checkProductBakset(idClient, ean)){
+                BasketDAO.updateBasket(idClient, ean);
+            }else{
+                BasketDAO.addProductToBasket(idClient, ean);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } 
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
