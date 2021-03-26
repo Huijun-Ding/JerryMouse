52ec193f020bb2794a6c87dea0179a8c5f5b7812
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-03-26 07:24:22 +0100

    Update department.js
    Grosse partie ! Correction et révision des différentes fonctions pour la prise en charges des rayons et des catégories

--- src/main/webapp/js/department.js
+++ src/main/webapp/js/department.js
@@ -3,22 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-function setDepartment(id, name) {
-    document.getElementById("departmentButton").data = name;
-    setViewOnDepartment(id);
-    getProductCategoriesByDepartmentId(id);
-}
-
-function setCategory(id, name) {
-    document.getElementById("categoryButton").data = name;
-    setViewOnCategory(id);
-}
-
 function getProductCategoriesByDepartmentId(departmentId) {
-    
     var xhr = new XMLHttpRequest();
-    
+
     var param = "departmentId=" + departmentId;
     var url = "../ProductCategory?" + param;
 
@@ -28,11 +15,21 @@ function getProductCategoriesByDepartmentId(departmentId) {
     // On précise ce que l'on va faire quand on aura reçu la réponse du serveur.
     xhr.onload = function ()
     {
+
         // Si la requête http s'est bien passée.
         if (xhr.status === 200) {
-            document.getElementById("productCategories_list").innerHtml = "";
             var categories = xhr.responseXML.getElementsByTagName("productCategory");
             var productCategories_list = document.getElementById("productCategories_list");
+            productCategories_list.innerHTML = "";
+            document.getElementById("categoryButton").innerHTML = "Toutes les catégories";
+            
+            var button = document.createElement("button");
+            button.innerHTML = "Toutes les catégories";
+            button.className = "dropdown-item";
+            button.addEventListener("click", function () {
+                document.getElementById("categoryButton").innerHTML = name;
+                setViewOnDepartment(departmentId);
+            });
             
             for (var i = 0; i < categories.length; i++) {
                 var id = categories[i].getElementsByTagName("id")[0].firstChild.nodeValue;
@@ -40,7 +37,11 @@ function getProductCategoriesByDepartmentId(departmentId) {
                 var button = document.createElement("button");
                 button.innerHTML = name;
                 button.className = "dropdown-item";
-                button.addEventListener("click", setCategory(id, name));
+                button.value = id;
+                button.addEventListener("click", function () {
+                    document.getElementById("categoryButton").innerHTML = this.innerHTML;
+                    setViewOnCategory(this.value);
+                });
                 productCategories_list.appendChild(button);
             }
         }
@@ -52,7 +53,7 @@ function getProductCategoriesByDepartmentId(departmentId) {
 
 function getDepartments() {
     var xhr = new XMLHttpRequest();
-    
+
     var url = "../Department";
 
     xhr.open("GET", url, true);
@@ -62,25 +63,82 @@ function getDepartments() {
     xhr.onload = function () {
         // Si la requête http s'est bien passée.
         if (xhr.status === 200) {
+            document.getElementById("departmentButton").innerHTML = "Tous les rayons";
+            document.getElementById("productCategories_list").innerHtml = "Toutes les catégories";
+
             document.getElementById("departments_list").innerHtml = "";
             var departments = xhr.responseXML.getElementsByTagName("department");
             var departments_list = document.getElementById("departments_list");
-            
+
+            var button = document.createElement("button");
+            button.innerHTML = "Tous les rayons";
+            button.className = "dropdown-item";
+            button.addEventListener("click", function () {
+                document.getElementById("departmentButton").innerHTML = "Tous les rayons";
+                document.getElementById("categoryButton").innerHTML = "Toutes les catégories";
+                document.getElementById("category").style.display = "none";
+                setViewOnHome();
+            });
+
+            departments_list.appendChild(button);
+
             for (var i = 0; i < departments.length; i++) {
                 var id = departments[i].getElementsByTagName("id")[0].firstChild.nodeValue;
                 var name = departments[i].getElementsByTagName("name")[0].firstChild.nodeValue;
                 var button = document.createElement("button");
                 button.innerHTML = name;
-                button.className = "dropdown-item";
-                button.addEventListener("click", setDepartment(id, name));
-                console.log("button.addEventListener('click', setDepartment(" + id + ", " + name  + "));")
+                button.className = "dropdown-item choose_department";
+                button.value = id;
                 departments_list.appendChild(button);
+            }
+
+            document.getElementById("category").style.display = "none";
+
+            var choose_store_buttons = document.getElementsByClassName("choose_department");
+
+            for (let i = 0; i < choose_store_buttons.length; i++) {
+                choose_store_buttons[i].addEventListener("click", function () {
+                    changeDepartment(choose_store_buttons[i].value);
+                });
             }
         }
     };
 
     // Envoie de la requête.
     xhr.send();
+}
+
+function changeDepartment(departmentId) {
+    // Objet XMLHttpRequest.
+    var xhr = new XMLHttpRequest();
+
+    // Requête au serveur avec les paramètres éventuels.
+    var param = "id=" + encodeURIComponent(departmentId);
+    var url = "../Department" + "?" + param;
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // On précise ce que l'on va faire quand on aura reçu la réponse du serveur.
+    xhr.onload = function () {
+        // Si la requête http s'est bien passée.
+        if (xhr.status === 200) {
+            var store = xhr.responseXML.getElementsByTagName("department")[0];
+            var id = store.getElementsByTagName("id")[0].firstChild.nodeValue;
+            var name = store.getElementsByTagName("name")[0].firstChild.nodeValue;
+            document.getElementById("departmentButton").innerHTML = name;
+
+            setViewOnDepartment(id);
+            getProductCategoriesByDepartmentId(id);
+        }
+    };
+
+    xhr.send(param);
+
+    document.getElementById("category").style.display = "block";
+}
+
+function setViewOnHome() {
+    document.getElementById("view").src = "../DisplayProducts?home";
 }
 
 function setViewOnDepartment(departmentId) {
