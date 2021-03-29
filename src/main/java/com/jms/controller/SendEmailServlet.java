b52a1c859acff7741f35756041b4ea4b9529c0b5
airol123 <63183652+airol123@users.noreply.github.com>
2021-03-30 00:21:37 +0200

    ajouter style email

--- src/main/java/com/jms/controller/SendEmailServlet.java
+++ src/main/java/com/jms/controller/SendEmailServlet.java
@@ -8,9 +8,21 @@ package com.jms.controller;
 import com.jms.dao.HibernateUtilDAO;
 import com.jms.model.Client;
 import com.jms.model.Order;
+import com.jms.model.OrderLine;
+import com.jms.model.OrderLineId;
+import com.jms.model.Product;
+import com.jms.model.ProductConditioning;
+import com.jms.model.ProductNutriScore;
+import com.jms.model.Store;
+import com.jms.model.TimeSlot;
 import com.jms.util.EmailUtil;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -41,18 +53,44 @@ public class SendEmailServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         //HttpSession session = request.getSession(true);
         // Client client = (Client) session.getAttribute("client");
-        Client client = new Client(2, "REN", "chloé", "18810952622@163.com", "123", 10);
+        Client client = new Client(2, "REN", "Chloé", "18810952622@163.com", "123", 10);
         String receiveMail = client.getEmail();
         // get order
         try ( Session sessionHiber = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             Transaction t = sessionHiber.beginTransaction();
             Order order = sessionHiber.load(Order.class, client.getCode());
-            t.commit(); 
+            t.commit();
         }
-        
-        String content = "test";
 
-        EmailUtil e = new EmailUtil(receiveMail, content,client.getFirstName()+client.getLastName());
+        Date d1 = new Date(2021, 4, 2, 7, 30);
+        Date d2 = new Date(2021, 4, 2, 8, 0);
+        Store s1=new Store("Auhchan", "rue du 12", "toulouse","31000");
+        TimeSlot t1 = new TimeSlot(d1, d2);
+        Product p1 = new Product("p1", "eau", 1.4f);
+        Product p2 = new Product("p2", "chocolat", 1.4f);
+        Order o1 = new Order("order1", client,s1,t1);
+        OrderLine ol1 = new OrderLine(2, o1, p1);
+        OrderLine ol2 = new OrderLine(1, o1, p2);
+        ArrayList<OrderLine> orderlines = new ArrayList<OrderLine>();
+        orderlines.add(ol1);
+        orderlines.add(ol2);
+        DateFormat format1 = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
+        String contentOrder = "<table style='width:100%' frame='above'><caption>Votre Facture"
+                + "</caption><thead><tr><th>Produit</th><th>Quantité</th><th>Montant</th></tr></thead>";
+        for (OrderLine ol : orderlines) {
+
+            contentOrder += "<tr><td align='center'>" + ol.getProduct().getName() + "</td><td align='center'>" 
+                    + ol.getQuantity() + "</td><td align='center'>" + ol.getProduct().getUnitPrice() * ol.getQuantity() + "</td></tr>";
+        }
+        contentOrder = contentOrder + "</table>";
+        String content = "<h4>Bonjour " + client.getFirstName()+"</h4></br>"+
+                "</br>Veuillez trouver ci-dessous un resumé des articles "
+                + "que vous avez commandés. Cet email vaut comme facture pour votre achat.</br> " 
+                + contentOrder+"</br> Adresse de Retrait : "+o1.getStore().getName()
+                +o1.getStore().getStreet()+", "+o1.getStore().getPostalCode()
+                +"<br>Date de Retrait : "+
+                format1.format(o1.getTimeslot().getStartTime())+" - "+format1.format(o1.getTimeslot().getEndTime());
+        EmailUtil e = new EmailUtil(receiveMail, content, client.getFirstName() + client.getLastName());
 
     }
 
