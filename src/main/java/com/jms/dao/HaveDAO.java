f242626046df6d333f636a98265770bb9e1489e9
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-03-29 22:17:37 +0200

    Update HaveDAO.java
    Création d'une méthode permettant d'initialiser les créneaux

--- src/main/java/com/jms/dao/HaveDAO.java
+++ src/main/java/com/jms/dao/HaveDAO.java
@@ -21,19 +21,20 @@ import org.hibernate.query.Query;
  * @author RAKOTOARISOA
  */
 public class HaveDAO {
+
     /*----- Date Format -----*/
     private static final String format = "yyyy-MM-dd";
     private static final SimpleDateFormat DF = new SimpleDateFormat(format);
-    
+
     /**
-     * 
+     *
      */
-    public static void create(String startTime, int storeId, int capacity, String date) throws ParseException  {
+    public static void create(String startTime, int storeId, int capacity, String date) throws ParseException {
         //Open a session
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             //Open a transaction
             Transaction t = session.beginTransaction();
-            
+
             HaveId haveId = new HaveId(startTime, storeId);
             Have d = new Have(haveId, capacity, DF.parse(date));
 
@@ -42,43 +43,44 @@ public class HaveDAO {
             t.commit();
         }
     }
-   
-    
-     public static void initialize() throws ParseException {
-        
-                        
-            int minCapacite = 5;
-            int maxCapacite = 20;
-            int range = maxCapacite - minCapacite + 1;
-            
-            for(Date d : DateUtil.nextDays(4)) {
-                for(int storeId = 1 ; storeId <= 43 ; storeId++) {
-                    int hour = 7;
-                    String minutes = "30";
-                    
-                    while(hour < 21) {
-                        int capacity = (int)(Math.random() * range) + minCapacite;
-                        
-                        String hoursMinutes = (hour < 10) ? "0" + hour + ":" + minutes : "" + hour + ":" + minutes;
-                                                
-                        if(minutes.equals("30")) hour++;
-                        minutes = (minutes.equals("00")) ? "30" : "00";
-                        
-                        create(hoursMinutes, storeId, capacity, DateUtil.yearMonthDayFormat(d));
+
+    public static void initialize() throws ParseException {
+
+        int minCapacite = 5;
+        int maxCapacite = 20;
+        int range = maxCapacite - minCapacite + 1;
+
+        for (int storeId = 1; storeId <= 43; storeId++) {
+            for (Date d : DateUtil.nextDays(4)) {
+                int hour = 7;
+                String minutes = "30";
+
+                while (hour < 21) {
+                    int capacity = (int) (Math.random() * range) + minCapacite;
+
+                    String hoursMinutes = (hour < 10) ? "0" + hour + ":" + minutes : "" + hour + ":" + minutes;
+
+                    if (minutes.equals("30")) {
+                        hour++;
                     }
+                    minutes = (minutes.equals("00")) ? "30" : "00";
+
+                    create(hoursMinutes, storeId, capacity, DateUtil.yearMonthDayFormat(d));
                 }
             }
-          
-     }
+        }
+
+    }
+
     /**
-     * 
+     *
      * @param storeId
      * @param datePickUp
      * @return
      */
-    public static List<Have> getTimeSlotsByStoreId(int storeId, Date datePickUp){
+    public static List<Have> getTimeSlotsByStoreId(int storeId, Date datePickUp) {
         //Open a session
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             //Open a transaction
             Transaction t = session.beginTransaction();
             Query query = session.createQuery("select new Have(h.date,h.capacity, h.store, h.timeSlot) "
@@ -89,12 +91,13 @@ public class HaveDAO {
 
             query.setParameter("storeId", storeId);
             query.setParameter("datePickUp", datePickUp);
-            
+
             return query.list();
         }
-    } 
-     
-    public static void main (String[] args) throws ParseException {
+    }
+
+    public static void main(String[] args) throws ParseException {
         initialize();
+        
     }
 }
