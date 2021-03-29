aa42324ea50eca5ae8407fd03ce01ac08686ba18
Shanshan-ZHAO <13051105108@163.com>
2021-03-29 23:25:10 +0200

    df.parse - df.format

--- src/main/java/com/jms/dao/TimeSlotDAO.java
+++ src/main/java/com/jms/dao/TimeSlotDAO.java
@@ -15,6 +15,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import java.text.DateFormat;
 
 /**
  *
@@ -35,11 +36,11 @@ public class TimeSlotDAO {
         try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             //Open a transaction
             Transaction t = session.beginTransaction();
-
-            TimeSlot t1 = new TimeSlot(DF.parse(startTime), DF.parse(endTime));
-
+    
+            TimeSlot t1 = new TimeSlot(DF.format(startTime), DF.format(endTime));
+    
             session.save(t1);
-
+    
             t.commit();
         }
     }
