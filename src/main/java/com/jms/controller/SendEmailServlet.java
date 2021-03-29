4b1c958b3d278dc93e00f3aea9de9fd84c96bd7d
airol123 <63183652+airol123@users.noreply.github.com>
2021-03-29 21:58:28 +0200

    servlet pour la finctionalité d'envoyer un mail

---
+++ src/main/java/com/jms/controller/SendEmailServlet.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jms.controller;
+
+import com.jms.dao.HibernateUtilDAO;
+import com.jms.model.Client;
+import com.jms.model.Order;
+import com.jms.util.EmailUtil;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Jerry Mouse Software.
+ */
+public class SendEmailServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        //HttpSession session = request.getSession(true);
+        // Client client = (Client) session.getAttribute("client");
+        Client client = new Client(2, "REN", "chloé", "18810952622@163.com", "123", 10);
+        String receiveMail = client.getEmail();
+        // get order
+        try ( Session sessionHiber = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = sessionHiber.beginTransaction();
+            Order order = sessionHiber.load(Order.class, client.getCode());
+            t.commit(); 
+        }
+        
+        String content = "test";
+
+        EmailUtil e = new EmailUtil(receiveMail, content,client.getFirstName()+client.getLastName());
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(SendEmailServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(SendEmailServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
