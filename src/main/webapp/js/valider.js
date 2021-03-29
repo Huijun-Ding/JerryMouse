8ce30c3b684e4e3dec043b4b4d43b0de5ba966fc
Shanshan-ZHAO <13051105108@163.com>
2021-03-29 23:22:53 +0200

    nv fonc pour valider une cmd

--- src/main/webapp/js/valider.js
+++ src/main/webapp/js/valider.js
@@ -8,18 +8,26 @@
  * This function is to validate a basket.
  */
 function validate() {
-    //var myinput = this.firstChild.nodeValue;
-
     // Objet XMLHttpRequest.
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "../Validate");
+    xhr.open("GET", "Validate");
 
-    xhr.onload = function ()
-    {
-        if (xhr.status === 200)
-        {
-            
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            msg_error = document.getElementById("msg_error");
+            msg_error.innerHTML = "";
+
+            result = xhr.responseXML.getElementsByTagName("results");
+            res = result[0].getElementsByTagName("res")[0].firstChild.nodeValue;
+            if(res === "ok"){
+                window.location.href = "confirmationOrder";
+            }else if(res === "connection"){
+                window.location.href = "login";
+            }else{
+                message = result[0].getElementsByTagName("message")[0].firstChild.nodeValue;
+                msg_error.insertAdjacentHTML('beforeend', message);
+            }
         }
     };
     xhr.send();
