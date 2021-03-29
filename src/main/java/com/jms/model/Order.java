fb728bf0d2efbf1d38508976605cc5b09e92adda
Miangola <rakotoarisoamiangola@gmail.com>
2021-03-29 18:25:44 +0200

    Update Order.java
    Add annotation generated value

--- src/main/java/com/jms/model/Order.java
+++ src/main/java/com/jms/model/Order.java
@@ -13,6 +13,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -32,6 +34,7 @@ public class Order {
      * The unique identity of a order.
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CodeCD")
     private int orderId;
     
