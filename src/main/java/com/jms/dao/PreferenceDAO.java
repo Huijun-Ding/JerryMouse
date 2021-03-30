a1a7bf03f6f8c1b2972996945d1ff561344f220b
malaikateuhi <malaikateuhi@gmail.com>
2021-03-30 14:24:02 +0200

    Create PreferenceDAO.java

---
+++ src/main/java/com/jms/dao/PreferenceDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jms.dao;
+
+import com.jms.model.Client;
+import com.jms.model.Product;
+import org.hibernate.Session;
+
+/**
+ * The PreferenceDAO class contains the DAO methods regarding the preferences
+ * of a client.
+ *
+ * @author Jerry Mouse Software
+ */
+public class PreferenceDAO {
+
+    
+}
