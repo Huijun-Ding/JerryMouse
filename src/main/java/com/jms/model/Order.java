a66862e37b4ef526b6f91f7b67e5c2fa1a36680a
airol123 <63183652+airol123@users.noreply.github.com>
2021-03-30 16:39:07 +0200

    rien

--- src/main/java/com/jms/model/Order.java
+++ src/main/java/com/jms/model/Order.java
@@ -7,9 +7,7 @@ package com.jms.model;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
