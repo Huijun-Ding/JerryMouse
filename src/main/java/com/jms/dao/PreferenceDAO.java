9f1c6d70ca6814331dc95b1d991dcc769aa8b42d
malaikateuhi <malaikateuhi@gmail.com>
2021-03-30 14:37:22 +0200

    Add method to set a product in favorite

--- src/main/java/com/jms/dao/PreferenceDAO.java
+++ src/main/java/com/jms/dao/PreferenceDAO.java
@@ -7,7 +7,9 @@ package com.jms.dao;
 
 import com.jms.model.Client;
 import com.jms.model.Product;
+import java.util.List;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  * The PreferenceDAO class contains the DAO methods regarding the preferences
@@ -17,5 +19,25 @@ import org.hibernate.Session;
  */
 public class PreferenceDAO {
 
+    /**
+     * Sets a product as favorite for a given client.
+     * @param product the product to add to favorites.
+     * @param client the given client.
+     */
+    public static void addFavoriteProduct(Product product, Client client) {
+        /*----- Session opening -----*/
+        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+            // Make objects persistents
+            session.saveOrUpdate(product);
+            session.saveOrUpdate(client);
+            
+            // Add in DB
+            client.getFavoriteProducts().add(product);
+            product.getClients().add(client);
+            
+            t.commit();
+            session.close();
+        }
     
 }
