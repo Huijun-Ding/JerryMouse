f691893fd096c4beef6b8a79224ebb349159b5f6
malaikateuhi <malaikateuhi@gmail.com>
2021-03-31 16:42:00 +0200

    Get favorite products lazily with persistence

--- src/main/java/com/jms/dao/PreferenceDAO.java
+++ src/main/java/com/jms/dao/PreferenceDAO.java
@@ -7,14 +7,15 @@ package com.jms.dao;
 
 import com.jms.model.Client;
 import com.jms.model.Product;
+import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 /**
- * The PreferenceDAO class contains the DAO methods regarding the preferences
- * of a client.
+ * The PreferenceDAO class contains the DAO methods regarding the preferences of
+ * a client.
  *
  * @author Jerry Mouse Software
  */
@@ -22,6 +23,7 @@ public class PreferenceDAO {
 
     /**
      * Sets a product as favorite for a given client.
+     *
      * @param product the product to add to favorites.
      * @param client the given client.
      */
@@ -32,39 +34,45 @@ public class PreferenceDAO {
             // Make objects persistents
             session.saveOrUpdate(product);
             session.saveOrUpdate(client);
-            
+
             // Add in DB
             client.getFavoriteProducts().add(product);
             product.getClients().add(client);
-            
+
             t.commit();
             session.close();
         }
     }
-    
+
     /**
      * Gets the favorite products of a client.
-     * @param client
-     * @return the list of favorite products of a client.
+     *
+     * @param client the given client.
+     * @return the client updated with the Set of favorite products.
      */
-    public static List<Product> getFavoriteProducts (Client client) {
-        List<Product> list;
+    public static Client getFavoriteProducts(Client client) {
+        //List<Product> list;
         /*----- Session opening -----*/
         try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             Transaction t = session.beginTransaction();
-            
-            String sql = "SELECT p "
-                    + "FROM Produit p "
-                    + "WHERE p.clients.code = :c";
-            
-            // Create HQL query
-            Query query = session.createQuery(sql);
-            query.setParameter("c", client.getCode());
-            list = query.list();
-            
-            t.commit();
-            session.close();
-            return list;
+
+            // Make the object in parameter persistent
+            session.persist(client);
+
+            // Get favorite products lazily
+            client.getFavoriteProducts();
+
+//            String sql = "SELECT p "
+//                    + "FROM Produit p "
+//                    + "WHERE p.clients.code = :c";
+//            
+//            // Create HQL query
+//            Query query = session.createQuery(sql);
+//            query.setParameter("c", client.getCode());
+//            list = query.list();
+            //t.commit();
+            //session.close();
+            return client;
         }
     }
 }
