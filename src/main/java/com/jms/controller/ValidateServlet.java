e92f6ced7ea6f2962cf3f7324b191e094a232f52
Shanshan-ZHAO <13051105108@163.com>
2021-03-31 20:27:59 +0200

    java doc throw

--- src/main/java/com/jms/controller/ValidateServlet.java
+++ src/main/java/com/jms/controller/ValidateServlet.java
@@ -44,7 +44,7 @@ public class ValidateServlet extends HttpServlet {
      *
      * @param request servlet request
      * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
+     * @throws ServletException if a servlet-specific  error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
