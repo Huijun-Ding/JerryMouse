de09e0d9cbf78d8677f26a40dc57f705cf369c79
Miangola <rakotoarisoamiangola@gmail.com>
2021-03-31 13:54:10 +0200

    Update ShoppingListFile.java
    Ajouter méthode pour lire le fichier et transformer chaque ligne en post-it, puis affecter à une liste déja créée

--- src/main/java/com/jms/util/ShoppingListFile.java
+++ src/main/java/com/jms/util/ShoppingListFile.java
@@ -5,7 +5,9 @@
  */
 package com.jms.util;
 
+import com.jms.model.Client;
 import com.jms.model.PostIt;
+import com.jms.model.ShoppingList;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -21,47 +23,54 @@ public class ShoppingListFile {
     /**
      * Read and turn each line of file into a PostIt
      *
-     * @param titleList
      * @param fileList
      */
-    public static void readFile(String titleList, File fileList) {
+    public static Set<PostIt> importFromFile( File fileList) {
         Set<PostIt> postIts = new HashSet<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileList));
             String ligne;
-            int nbLigne = 0;
-            //Ne pas lire le titre
-            int numLigne = 1;
+            int nbLigne = 1;
             while ((ligne = br.readLine()) != null) {
                 System.out.println(ligne);
-                nbLigne++;
-                while (numLigne < nbLigne) {
-                    postIts.add(new PostIt(ligne));
-                    numLigne++;
-                }
+                postIts.add(new PostIt(nbLigne, ligne));
+                nbLigne++;  
             }
+            
+            
             System.out.println(nbLigne);
-            for (PostIt postIt : postIts) {
-                System.out.println(postIt);
-            }
-
+//            for (PostIt postIt : postIts) {
+//                System.out.println(postIt);
+//            }
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return postIts;
+    }
+    
+    public static ShoppingList addPostIt(String titleList, Set<PostIt> postIts, File fileList ){
+        
+        postIts = importFromFile(fileList);
+        ShoppingList fileImported =  new ShoppingList(titleList,postIts);
+        return fileImported;
     }
 
     public static void main(String[] args) {
-        //readFile("Liste Apéro", new File("listeShopping.txt"));
-        Set<PostIt> sets = new HashSet<>(0);
-        PostIt p = new PostIt("Fraise");
-        PostIt p1 = new PostIt("Pomme");
+        File list = new File("listeShopping.txt");
+        Set<PostIt> postIts =  importFromFile(list);
         
-        System.out.println(sets.add(p));
-        //return false car code=0 existe déja
-        System.out.println(sets.add(p1));
-        sets.add(p1);
-
-
+        ShoppingList s = addPostIt("Liste apéro",postIts,list);
+        System.out.println(s.getPostIts());
+        
+//        Set<PostIt> sets = new HashSet<>(0);
+//        PostIt p = new PostIt("Fraise");
+//        PostIt p1 = new PostIt("Pomme");
+//        
+//        System.out.println(sets.add(p));
+//        //return false car code=0 existe déja
+//        System.out.println(sets.add(p1));
+//        sets.add(p1);
 
     }
 }
