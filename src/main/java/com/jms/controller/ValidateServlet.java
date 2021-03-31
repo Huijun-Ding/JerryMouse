c1d861623793cc7499e84bad365ce5fe995d3f85
Shanshan-ZHAO <13051105108@163.com>
2021-03-31 20:22:45 +0200

    rien

--- src/main/java/com/jms/controller/ValidateServlet.java
+++ src/main/java/com/jms/controller/ValidateServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jms.controller;
+package com.jms.controller; 
 
 import com.jms.dao.BasketDAO;
 import com.jms.dao.ClientDAO;
