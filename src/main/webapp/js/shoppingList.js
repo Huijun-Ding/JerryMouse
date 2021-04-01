23c5a2806e6db955e49ff0848084072f073aa6e4
Huijun <mathilda.cnfr@gmail.com>
2021-04-01 15:47:36 +0200

    change access rigne to shopping list

--- src/main/webapp/js/shoppingList.js
+++ src/main/webapp/js/shoppingList.js
@@ -1,26 +1,3 @@
-function displayCreateShoppingList() {
-    // Objet XMLHttpRequest.
-    var xhr = new XMLHttpRequest();
-
-    // Requête au serveur avec les paramètres éventuels.
-    xhr.open("GET", "CheckLogin");
-
-    xhr.onload = function ()
-    {
-        if (xhr.status === 200)
-        {
-            var idClients = xhr.responseXML.getElementsByTagName("idClient");
-            if (idClients[0].firstChild.nodeValue === "no") {
-                window.location.href = "login";
-
-            }
-        }
-    };
-
-    // Envoie de la requête.
-    xhr.send();
-}
-
 function displayShoppingLists() {
     // relocated to page myShoppingLists
     window.location.href = "myShoppingLists";
@@ -45,11 +22,6 @@ function sendShoppingListName() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (window.addEventListener) {
-        window.addEventListener('load', displayCreateShoppingList); //W3C
-    } else {
-        window.attachEvent('onload', displayCreateShoppingList); //IE
-    }
 
     var btn = document.getElementById("shopping_list_button");
     if (btn !== null) {
