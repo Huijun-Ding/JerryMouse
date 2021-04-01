a60887be09c1f76d11cd7f6fe8770bee84ca534b
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-04-01 15:22:18 +0200

    Update FileUtil.java
    Correction pour la prise en charge des accents et  le retrait d'éléments inutiles comme des espaces en trop. Retrait des caractères autres que de l'alphanumérique.

--- src/main/java/com/jms/util/FileUtil.java
+++ src/main/java/com/jms/util/FileUtil.java
@@ -13,6 +13,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -31,27 +33,46 @@ public class FileUtil {
             BufferedReader br = new BufferedReader(new FileReader(fileList));
             String ligne;
             while ((ligne = br.readLine()) != null) {
-                postIts.add(new PostIt(ligne));
+                String myPostIt = new String(ligne.getBytes(), "UTF-8");
+                
+                String[] sentence = myPostIt.split("\\s");
+                myPostIt = "";
+                for(int i = 0 ; i < sentence.length ; i++) {
+                    sentence[i] = sentence[i].replaceAll("[^\\w'éàèùâêîôûÀÂÆÇÈÉÊÄËÏÖÜÂÊÎÔÛŒÙŸäëïöüçœæ]", " ");
+                    
+                    boolean b = Pattern.matches("^(\\s)*$", sentence[i]);
+                    
+                    myPostIt += (sentence[i].equals("") || b) ? "" : " " + sentence[i];
+                }
+                
+                // Remove all spaces characters after and before string
+                Pattern p = Pattern.compile("^\\s*(.*)\\s*$");
+                Matcher m = p.matcher(myPostIt);
+                myPostIt = (m.matches()) ? m.group(1) : myPostIt;
+                
+                if(!myPostIt.equals(""))
+                    postIts.add(new PostIt(myPostIt));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return postIts;
     }
-    
+
     public static ShoppingList importShoppingListFromFile(String titleList, File file) {
         Set<PostIt> postIts = importPostItsFromFile(file);
         ShoppingList fileImported = new ShoppingList(titleList, postIts);
-        
-        for (PostIt p : postIts) {
+
+        for(PostIt p : postIts) {
             p.setShoppingList(fileImported);
         }
-        
+
         return fileImported;
     }
-    
+
     public static void main(String[] args) {
-        File file = new File("listeShopping.txt");        
+        File file = new File("nb-configuration.xml");
         ShoppingList s = importShoppingListFromFile("Liste apéro", file);
         System.out.println(s);
         System.out.println(s.getPostIts());
@@ -59,7 +80,7 @@ public class FileUtil {
 //        Set<PostIt> sets = new HashSet<>(0);
 //        PostIt p = new PostIt("Fraise");
 //        PostIt p1 = new PostIt("Pomme");
-//        
+//
 //        System.out.println(sets.add(p));
 //        //return false car code=0 existe déja
 //        System.out.println(sets.add(p1));
