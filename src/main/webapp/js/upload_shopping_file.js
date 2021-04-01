ca10c461ca200cfeef4a654e9159669d9a1cdc57
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-04-01 15:21:06 +0200

    Update upload_shopping_file.js
    Lecture des fichiers par le serveur et récupération par le navigateur.
    Affichage d'un message d'erreur si le type de fichier n'est ni un csv, ni un txt.

--- src/main/webapp/js/upload_shopping_file.js
+++ src/main/webapp/js/upload_shopping_file.js
@@ -7,21 +7,47 @@
 function upload() {
     var elt = document.getElementById("fileShoppingList");
     var files = elt.files;
-    
-    var formData = new FormData();
-    formData.append("file", files[0], files[0].name);
-    formData.append("nameList", "Test 31_03_2021");
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "UploadShoppingFile", true);
-    
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            
-        }
-    };
 
-    xhr.send(formData);
+    var formData = new FormData();
+    if (files[0] !== null) {
+        formData.append("file", files[0], files[0].name);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "UploadShoppingFile", true);
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var htmlText = "";
+                
+                var msg_error = xhr.responseXML.getElementsByTagName("msg_error");
+                
+                if(msg_error.length === 0) {
+                    var postIts = xhr.responseXML.getElementsByTagName("postIt");
+
+                    for (var i = 0; i < postIts.length; i++) {
+                        var wording = postIts[i].getElementsByTagName("wording")[0].firstChild.nodeValue;
+                        htmlText += '<input type="text" name="postIts" class="form-control my-2"';
+                        htmlText += 'value="' + wording + '" placeholder="' + wording + '">\n';
+                    }
+                } else {
+                    var title = xhr.responseXML.getElementsByTagName("title")[0].firstChild.nodeValue;
+                    var content = xhr.responseXML.getElementsByTagName("content")[0].firstChild.nodeValue;
+                    htmlText += '<div class="alert alert-danger alert-dismissible fade show" role="alert">\n';
+                    htmlText += '   <strong>' + title + '</strong> ' + content +'\n';
+                    htmlText += '   <button type="button" class="btn_close" data-dismiss="alert" aria-label="Close">\n';
+                    htmlText += '       <span aria-hidden="true">&times;</span>\n';
+                    htmlText += '   </button>\n';
+                    htmlText += '</div>\n';
+                }
+
+                document.getElementById("post_its_from_file").innerHTML = htmlText;
+            }
+        };
+
+        xhr.send(formData);
+    } else {
+        document.getElementById("post_its_from_file").innerHTML = "";
+    }
 }
 
 /**
