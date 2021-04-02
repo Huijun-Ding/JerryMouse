ad63ae135257e91e2986f22cf6a304413937b167
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-04-02 07:19:05 +0200

    Add method create a shopping list from a set of post-its
    Method ok + javadoc + comments
    Test in main

--- src/main/java/com/jms/dao/ShoppingListDAO.java
+++ src/main/java/com/jms/dao/ShoppingListDAO.java
@@ -7,6 +7,7 @@ import com.jms.model.Product;
 import com.jms.model.ShoppingList;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.hibernate.Session;
@@ -17,7 +18,7 @@ public class ShoppingListDAO {
 
     public static void saveShoppingList(String name, Client client) {
 
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             // new  session
             Transaction t = session.beginTransaction();
 
@@ -31,8 +32,8 @@ public class ShoppingListDAO {
     }
 
     public static List<ShoppingList> getMyShoppingLists(int idClient) throws SQLException {
-        
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             Transaction t = session.beginTransaction();
             Query query = session.createQuery("from ListeCourse where CodeCL = :id");
 
@@ -48,7 +49,7 @@ public class ShoppingListDAO {
 
     public static void savePostIt(String namePostIt, ShoppingList shoppinglist) {
 
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             // new  session
             Transaction t = session.beginTransaction();
 
@@ -63,7 +64,7 @@ public class ShoppingListDAO {
 
     public static void savePostItWithProduct(Product product, ShoppingList shoppinglist) {
 
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             // new  session
             Transaction t = session.beginTransaction();
 
@@ -77,7 +78,7 @@ public class ShoppingListDAO {
     }
 
     public static ShoppingList getShoppingList(int id) {
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             session.beginTransaction();
 
             Query query = session.createQuery("from ListeCourse where CodeLC = :id");
@@ -92,7 +93,7 @@ public class ShoppingListDAO {
 
     public static List<PostIt> getPostIts(int idShoppinglist) throws SQLException {
 
-        try (Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
             Transaction t = session.beginTransaction();
 
             Query query = session.createQuery("from PostIt where CodeLC = :id");
@@ -107,16 +108,51 @@ public class ShoppingListDAO {
         }
     }
 
+    /**
+     * Creates in database a shopping list for a given client, with a collection
+     * of post-its.
+     *
+     * @param title the name of the shopping list.
+     * @param postIts the set of post-its to add to the shopping list.
+     * @param client the client creating the shoppinf list.
+     */
+    public static void create(String title, Set<PostIt> postIts, Client client) {
+
+        try ( Session session = HibernateUtilDAO.getSessionFactory().getCurrentSession()) {
+            Transaction t = session.beginTransaction();
+            // Create a new shopping list with parameters
+            ShoppingList s = new ShoppingList(title, postIts, client);
+            // Make the shopping list persistent in DB
+            session.save(s);
+            // Save each post-it in DB with the given shopping list
+            for (PostIt p : s.getPostIts()) {
+                p.setShoppingList(s);
+                session.save(p);
+            }
+            session.update(s);
+
+            t.commit();
+            session.close();
+        }
+    }
+
     public static void main(String[] args) throws ParseException, SQLException {
         // Test
         // ShoppingListDAO.saveShoppingList("cooktail");
         // ShoppingListDAO.getMyShoppingLists(2); 
-        List<PostIt> lstPostIts = ShoppingListDAO.getPostIts(5);
-        for (PostIt lstPostIt : lstPostIts) {
-            if(lstPostIt.getProduct() != null)
-                System.out.println("Product name : " + lstPostIt.getProduct().getName());
-        }
+        //List<PostIt> lstPostIts = ShoppingListDAO.getPostIts(5);
+//        for (PostIt lstPostIt : lstPostIts) {
+//            if(lstPostIt.getProduct() != null)
+//                System.out.println("Product name : " + lstPostIt.getProduct().getName());
+//        }
         //ShoppingListDAO.getShoppingList(5);
+        Client client = ClientDAO.load(2);
+        Set<PostIt> postIts = new HashSet<>(0);
+
+        postIts.add(new PostIt("vin 1"));
+        postIts.add(new PostIt("beurre"));
+        postIts.add(new PostIt("farine"));
+        ShoppingListDAO.create("Test Create", postIts, client);
 
         // Exit
         System.exit(0);
