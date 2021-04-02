f238d9ded1b72ec62d3027b12ae1d3b98f790fba
malaikateuhi <malaikateuhi@gmail.com>
2021-04-02 10:22:09 +0200

    Update favorites.js

--- src/main/webapp/js/favorites.js
+++ src/main/webapp/js/favorites.js
@@ -26,3 +26,35 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+window.onbeforeunload = function getNumberOfItems() {
+    var x = screen.width;
+    //alert ("x = " + x);
+    var max;
+    if (x >= 992) {
+        max = 4;
+    } else if(x >= 768) {
+        max = 3;
+    } else max = 1;
+    
+    alert("window.location.href = " + window.location.href);
+    
+//    var xhr = new XMLHttpRequest();
+    var param = "max="+max;
+//    var url = "DisplayProducts" + "?home&" + param;
+//    
+//    xhr.open("GET", url, true);
+//    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//    
+//    xhr.send(param);
+    var queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("max", max);
+    history.pushState(null, null, "?" + queryParams.toString());
+    //window.location.search = param;
+    alert("location.href = " + location.href);
+}
+
+//document.onload = getNumberOfItems();
+document.addEventListener("DOMContentLoaded", () => {
+    //getNumberOfItems();
+});
