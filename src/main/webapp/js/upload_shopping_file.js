7d7f355254a75712c581b190f67556cf2d542d26
Axel TOA <axeltoa.edusup@yahoo.fr>
2021-04-02 09:51:29 +0200

    Update upload_shopping_file.js
    Mise à jour de la fonction javascript pour enregistrer une liste de courses à partir d'un fichier TXT.

--- src/main/webapp/js/upload_shopping_file.js
+++ src/main/webapp/js/upload_shopping_file.js
@@ -9,7 +9,7 @@ function upload() {
     var files = elt.files;
 
     var formData = new FormData();
-    if (files[0] !== null) {
+    if (files[0] !== null && files[0] !== undefined) {
         formData.append("file", files[0], files[0].name);
 
         var xhr = new XMLHttpRequest();
@@ -18,10 +18,10 @@ function upload() {
         xhr.onload = function () {
             if (xhr.status === 200) {
                 var htmlText = "";
-                
+
                 var msg_error = xhr.responseXML.getElementsByTagName("msg_error");
-                
-                if(msg_error.length === 0) {
+
+                if (msg_error.length === 0) {
                     var postIts = xhr.responseXML.getElementsByTagName("postIt");
 
                     for (var i = 0; i < postIts.length; i++) {
@@ -33,9 +33,9 @@ function upload() {
                     var title = xhr.responseXML.getElementsByTagName("title")[0].firstChild.nodeValue;
                     var content = xhr.responseXML.getElementsByTagName("content")[0].firstChild.nodeValue;
                     htmlText += '<div class="alert alert-danger alert-dismissible fade show" role="alert">\n';
-                    htmlText += '   <strong>' + title + '</strong> ' + content +'\n';
-                    htmlText += '   <button type="button" class="btn_close" data-dismiss="alert" aria-label="Close">\n';
-                    htmlText += '       <span aria-hidden="true">&times;</span>\n';
+                    htmlText += '   <strong>' + title + '</strong> ' + content + '\n';
+                    htmlText += '   <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">\n';
+                    htmlText += '       <span aria-hidden="true"></span>\n';
                     htmlText += '   </button>\n';
                     htmlText += '</div>\n';
                 }
@@ -50,9 +50,89 @@ function upload() {
     }
 }
 
+function save() {
+    var formData = new FormData();
+
+    var title = document.getElementById("import_title_list").value;
+    var postIts = document.getElementsByName("postIts");
+
+    formData.append("title", title);
+
+    for (var i = 0; i < postIts.length - 1 ; i++) {
+        formData.append("postIts", postIts[i].value);
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "UploadShoppingFile", true);
+
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var htmlText = "";
+
+            var msg_error = xhr.responseXML.getElementsByTagName("msg_error");
+            var title = xhr.responseXML.getElementsByTagName("title")[0].firstChild.nodeValue;
+            var content = xhr.responseXML.getElementsByTagName("content")[0].firstChild.nodeValue;
+
+            if (msg_error.length === 0) {
+                var msg_success = '<div class="alert alert-success alert-dismissible fade show" role="alert">\n';
+                msg_success += '   <strong>' + title + ' :"</strong> ' + content + '\n';
+                msg_success += '   <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">\n';
+                msg_success += '       <span aria-hidden="true"></span>\n';
+                msg_success += '   </button>\n';
+                msg_success += '</div>\n';
+                
+                elt.innerHTML = msg_success;
+                
+                printShoppingLists();
+            } else {
+                htmlText += '<div class="alert alert-danger alert-dismissible fade show" role="alert">\n';
+                htmlText += '   <strong>' + title + '</strong> ' + content + '\n';
+                htmlText += '   <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">\n';
+                htmlText += '       <span aria-hidden="true"></span>\n';
+                htmlText += '   </button>\n';
+                htmlText += '</div>\n';
+            }
+
+            document.getElementById("post_its_from_file").innerHTML = htmlText;
+        }
+    };
+
+    xhr.send(formData);
+}
+
+function printShoppingLists() {
+    // Objet XMLHttpRequest.
+    var xhr2 = new XMLHttpRequest();
+
+    xhr2.open("GET", "DisplayAllShoppingLists");
+
+    xhr2.onload = function ()
+    {
+        if (xhr2.status === 200)
+        {
+            suggestions = xhr2.responseXML.getElementsByTagName("shoppingList");
+            elt = document.getElementById("my_lists");
+
+            if (suggestions !== "") {
+                for (i = 0; i < suggestions.length; i++) {
+                    shoppingListLine = suggestions[i];
+
+                    code = shoppingListLine.getElementsByTagName("code")[0].firstChild.nodeValue;
+                    name = shoppingListLine.getElementsByTagName("name")[0].firstChild.nodeValue;
+
+                    elt.insertAdjacentHTML("beforeend", "<a href='showShoppingList?codesl=" + code + "' class='list-group-item list-group-item-action'>" + name + "</a>");
+                }
+            }
+        }
+    };
+
+    xhr2.send();
+}
+
 /**
  * Lancement après le chargement du DOM.
  */
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("fileShoppingList").addEventListener("change", upload);
+    document.getElementById("validate_uploading_file").addEventListener("click", save);
 });
